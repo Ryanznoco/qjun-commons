bd8cb8aee52f9fde44e27cc2636a4b7923048326
renqiang <810625938@qq.com>
2019-01-11 17:00:14 +0800

    修改注释

--- src/main/java/cn/com/qjun/common/util/UUIDUtils.java
+++ src/main/java/cn/com/qjun/common/util/UUIDUtils.java
@@ -6,16 +6,25 @@ import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 /**
- * @author Ryan
- * @date 2018-12-27
+ * @author RenQiang
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UUIDUtils {
 
+    /**
+     * 生成32位UUID
+     *
+     * @return 32位UUID
+     */
     public static String uuid() {
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
+    /**
+     * 生成36位标准UUID
+     *
+     * @return 36位标准UUID
+     */
     public static String standard() {
         return UUID.randomUUID().toString();
     }
